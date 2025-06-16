d09ea14962f0122bd2d61355759e83cad8595e3b
Christmasw <122515437+Christmasw@users.noreply.github.com>
2025-06-16 21:56:34 +0700

    Update script.js

--- src/SignIn/script.js
+++ src/SignIn/script.js
@@ -1,3 +1,64 @@
+$(document).ready(function() {
+    if(localStorage.length > 0) {
+        for (let i = 0; i < localStorage.length; i++) {
+            const key = localStorage.key(i);
+            const value = localStorage.getItem(key);
+            
+            if(JSON.parse(value).status == "active") {
+                window.location.href = "/Free-E-Library/src/Home/Science.html"
+            }
+        }
+    }
+});
+
 const clickLogin = () => {
-    window.location.href = "/Free-E-Library/src/Home/science.html"
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+
+    let id = "";
+    let getName = "";
+    let getTelp = "";
+    let getEmail = "";
+    let getPassword = "";
+
+    if (localStorage.length > 0) {
+        for (let i = 0; i < localStorage.length; i++) {
+            const key = localStorage.key(i);
+            const value = localStorage.getItem(key);
+            
+            if(email == JSON.parse(value).email) {
+                id = key,
+                getName = JSON.parse(value).name,
+                getTelp = JSON.parse(value).telp,
+                getEmail = JSON.parse(value).email
+                getPassword = JSON.parse(value).password
+            }
+        }
+
+        if (password != "" && password == getPassword) {
+            const userData = {
+                name: getName,
+                telp: getTelp,
+                email: getEmail,
+                password: getPassword,
+                status: "active",
+            };
+            localStorage.setItem(id, JSON.stringify(userData));
+
+            window.location.href = "/Free-E-Library/src/Home/Science.html"
+        }
+        else {
+            const validateAccount = document.getElementById('active-warning');
+            validateAccount.style.display = "block"
+        }
+    }
+    else {
+        const validateAccount = document.getElementById('active-warning');
+        validateAccount.style.display = "block"
+    }
+}
+
+const closeWarning = () => {
+    const validateAccount = document.getElementById('active-warning');
+    validateAccount.style.display = "none"
 }
